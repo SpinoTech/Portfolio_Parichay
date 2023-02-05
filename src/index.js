c031f979e072790b560370f0beadc6ba059ec3c1
parichay haldar <88630085+SpinoTech@users.noreply.github.com>
2023-02-05 18:00:06 +0530

    2nd try

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import {  
-  BrowserRouter as Router,  
+  HashRouter as Router,  
   // Routes,  
   // Route,  
   // Link  
